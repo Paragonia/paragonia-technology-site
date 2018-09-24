789a9185c1907151cf30d1a33913219e7d9743de
Andre Kampert <andre@paragonia.nl>
2018-09-24 23:27:08 +0200

    Add contact section

---
+++ src/components/contact.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt, faMobileAlt, faAt } from '@fortawesome/free-solid-svg-icons'
+
+const Contact = () => {
+    return (
+        <div className="content-blocks">
+            <section className="content">
+                <div className="block-content">
+                    <h3 className="title">Get in touch</h3>
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <form id="contactForm" data-toggle="validator" className="contact-from shake" name="contact" netlify="true">
+                                <div className="form-group">
+                                    <input id="name" type="text" className="form-control" name="Name" autoComplete="off" required data-error="Please enter your name" placeholder="* Your Name"/>
+                                    <div className="help-block with-errors"></div>
+                                </div>
+                                <div className="form-group">
+                                    <input id="email" type="email" className="form-control" name="Email" autoComplete="off" required data-error="Please enter your email" placeholder="* Your Email"/>
+                                    <div className="help-block with-errors"></div>
+                                </div>
+                                <div className="form-group">
+                                    <textarea id="message" className="form-control textarea" rows="10" name="Message" required data-error="Please enter your message subject" placeholder="* Your Message"></textarea>
+                                    <div className="help-block with-errors"></div>
+                                </div>
+                                <div className="form-group">
+                                    <button id="submit" type="submit" className="btn selected">Send Message</button>
+                                    <div id="msgSubmit" className="h3 text-center hidden"></div>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div className="col-lg-5 offset-lg-1">
+                            <div className="contact-content">
+                                <div className="contact-icon">
+                                    <FontAwesomeIcon icon={ faMapMarkerAlt } />
+                                </div>
+
+                                <div className="contact-details">
+                                    <h5>Address</h5>
+                                    <p>
+                                        Madagaskarstraat 5<br/>
+                                        1339 RD Almere
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="contact-content">
+                                <div className="contact-icon">
+                                    <FontAwesomeIcon icon={ faMobileAlt } />
+                                </div>
+                                <div className="contact-details">
+                                    <h5>Call Us</h5>
+                                    <p> <a href="tel:+31648260837">+31 6 48 26 08 37‬</a></p>
+                                </div>
+                            </div>
+                            <div className="contact-content">
+                                <div className="contact-icon">
+                                    <FontAwesomeIcon icon={ faAt } />
+                                </div>
+                                <div className="contact-details">
+                                    <h5>Email</h5>
+                                    <p>info<span style={{display: 'none'}}>-</span>@<span style={{display: 'none'}}>-</span>paragonia.nl</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+     )
+}
+
+export default Contact
--- src/components/profile.js
+++ src/components/profile.js
@@ -6,6 +6,9 @@ import imgAvatar2x from '../images/avatar/avatar-390x390-2x.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
+import About from '../components/about'
+import Contact from '../components/contact'
+
 import profileStyles from './profile.module.css'
 
 const Profile = () => {
@@ -39,28 +42,8 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className="col-sm-8 huge-side right-side">
-                    <section className="content">
-                        <div className="block-content">
-                            <h3 className="title">About Me</h3>
-                            <h3 className="subtitle">I am a <b>Full Stack Software Engineer</b> located near Amsterdam, the Netherlands.</h3>
-                            <p>
-                                Specialised in the design and implementation of large-scale complex systems.
-                                I enjoy working with independent inter-disciplinary teams and collaborative organizations.
-                                I have been working actively on the areas of business analysis, interaction design, functional modeling, enterprise architecture, technical design, and full stack software development.
-                                My principal interests revolve around the use of technology to develop flexible organizations and products that are responsive to change.
-                                I have a genuine interest in all software technology, and use my broad experience to make life easier for the people around me, while encouraging others to do the same.
-                            </p>
-                            <div className={ `row ${profileStyles.infoList}` }>
-                                <p className="col-sm-6"><strong>Name: </strong> André Kampert</p>
-                                <p className="col-sm-6"><strong>Email: </strong> andre@paragonia.nl</p>
-                                <p className="col-sm-6"><strong>Phone: </strong> + 31 6 48 26 08 37</p>
-                                <p className="col-sm-6">
-                                    <strong>Status: </strong>
-                                    <span className={ profileStyles.available }>Available soon</span>
-                                </p>
-                            </div>
-                        </div>
-                    </section>
+                    <About/>
+                    <Contact/>
                 </div>
             </div>
         </div>
