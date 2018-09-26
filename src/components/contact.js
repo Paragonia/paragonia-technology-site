b60c96accecb85e4e21ec486086bec0be60fc493
Andre Kampert <andre@paragonia.nl>
2018-09-26 09:23:58 +0200

    Remove contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,24 +11,24 @@ const Contact = () => {
                     <h3 className="title">Get in touch</h3>
                     <div className="row">
                         <div className="col-lg-6">
-                            <form id="contactForm" data-toggle="validator" className="contact-from shake" name="contact" netlify="true">
-                                <div className="form-group">
-                                    <input id="name" type="text" className="form-control" name="Name" autoComplete="off" required data-error="Please enter your name" placeholder="* Your Name"/>
-                                    <div className="help-block with-errors"></div>
+                            <div className="contact-content">
+                                <div className="contact-icon">
+                                    <FontAwesomeIcon icon={ faMobileAlt } />
                                 </div>
-                                <div className="form-group">
-                                    <input id="email" type="email" className="form-control" name="Email" autoComplete="off" required data-error="Please enter your email" placeholder="* Your Email"/>
-                                    <div className="help-block with-errors"></div>
+                                <div className="contact-details">
+                                    <h5>Call me</h5>
+                                    <p> <a href="tel:+31648260837">+31 6 48 26 08 37‬</a></p>
                                 </div>
-                                <div className="form-group">
-                                    <textarea id="message" className="form-control textarea" rows="10" name="Message" required data-error="Please enter your message subject" placeholder="* Your Message"></textarea>
-                                    <div className="help-block with-errors"></div>
+                            </div>
+                            <div className="contact-content">
+                                <div className="contact-icon">
+                                    <FontAwesomeIcon icon={ faAt } />
                                 </div>
-                                <div className="form-group">
-                                    <button id="submit" type="submit" className="btn selected">Send Message</button>
-                                    <div id="msgSubmit" className="h3 text-center hidden"></div>
+                                <div className="contact-details">
+                                    <h5>Email</h5>
+                                    <p>info<span style={{display: 'none'}}>-</span>@<span style={{display: 'none'}}>-</span>paragonia.nl</p>
                                 </div>
-                            </form>
+                            </div>
                         </div>
 
                         <div className="col-lg-5 offset-lg-1">
@@ -43,24 +43,6 @@ const Contact = () => {
                                         Madagaskarstraat 5<br/>
                                         1339 RD Almere
                                     </p>
-                                </div>
-                            </div>
-                            <div className="contact-content">
-                                <div className="contact-icon">
-                                    <FontAwesomeIcon icon={ faMobileAlt } />
-                                </div>
-                                <div className="contact-details">
-                                    <h5>Call Us</h5>
-                                    <p> <a href="tel:+31648260837">+31 6 48 26 08 37‬</a></p>
-                                </div>
-                            </div>
-                            <div className="contact-content">
-                                <div className="contact-icon">
-                                    <FontAwesomeIcon icon={ faAt } />
-                                </div>
-                                <div className="contact-details">
-                                    <h5>Email</h5>
-                                    <p>info<span style={{display: 'none'}}>-</span>@<span style={{display: 'none'}}>-</span>paragonia.nl</p>
                                 </div>
                             </div>
                         </div>
