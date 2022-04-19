b068bb204062cd4a8c7f83fd2421e501e8b82d6b
Andre Kampert <andre@paragonia.nl>
2022-04-19 11:45:22 +0200

    Disable plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,18 +12,6 @@ module.exports = {
     },
     'gatsby-transformer-remark',
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'Paragonia Technology B.V.',
-        short_name: 'paragonia',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/paragonia-logo.png', // This path is relative to the root of the site.
-      },
-    },
     'gatsby-plugin-offline',
   ],
 }
